8a406ccc332a4edde83bfc98099fc202ac1dbb16
Goshi <33128523+Goshi126@users.noreply.github.com>
2018-02-01 00:27:49 +0100

    Main 0.1 non fonctionnel
    Il faudrait que quelqu'un test ça chez lui car je crois que j'ai encore des problèmes de linkers malgré le fait que "tout était réglé".
    Il est nécessaire de créer un répertoire "images" à coté du main et de mettre "Sprites_Pac_Man_King_Arthur_0_1.png" dedans pour tester ce code.

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace std;
+using namespace sf;
+
+int main()
+{
+    RenderWindow window(VideoMode(900, 600), "PacMan_alpha_0.1");
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+
+    Texture sheet;
+    Sprite pac_man;
+
+    if (!sheet.loadFromFile("images\\Sprites_Pac_Man_King_Arthur_0_1.png",IntRect(10, 10, 32, 32))) // Si le chargement du fichier a échoué
+    {
+        cout<<"Erreur durant le chargement de la texture"<<endl;
+        return EXIT_FAILURE; // On ferme le programme
+    }
+
+    if (!sheet.create(200, 200))
+    {
+        cout<<"Erreur durant la création de la texture"<<endl;
+        return EXIT_FAILURE; // On ferme le programme
+    }
+
+    pac_man.setTexture(sheet);
+
+    while (window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+
+        window.draw(pac_man);
+
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
