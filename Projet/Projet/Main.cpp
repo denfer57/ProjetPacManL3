833f7e11fe7d1f493653d60b2c69f4c84a91ac00
denfer57 <jordan.fromeyer@gmail.com>
2018-03-07 15:13:20 +0100

    Affichage de la map

--- Projet/Projet/Main.cpp
+++ Projet/Projet/Main.cpp
@@ -3,52 +3,80 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 
 using namespace std;
 using namespace sf;
 
 int main()
 {
-	RenderWindow window(VideoMode(900, 600), "PacMan_alpha_0.1");
-	CircleShape shape(100.f);
-	shape.setFillColor(Color::Green);
+	RenderWindow window(VideoMode(770, 770), "PacMan_alpha_0.1");
+	window.setKeyRepeatEnabled(false);
 
 	Texture sheet;
+	Texture sheet2;
 	Sprite pac_man;
+	Sprite fond;
 
-	if (!sheet.loadFromFile("images\\Sprites_Pac_Man_King_Arthur_0_1.png", IntRect(10, 10, 32, 32))) // Si le chargement du fichier a échoué
+	if (!sheet.loadFromFile("images\\Sprites_Pac_Man_King_Arthur_0_1.png", IntRect(4, 10, 16, 16))) // Si le chargement du fichier a échoué
 	{
 		cout << "Erreur durant le chargement de la texture" << endl;
 		return EXIT_FAILURE; // On ferme le programme
 	}
 
-	if (!sheet.create(200, 200))
+	if(!sheet2.loadFromFile("images\\Pac_man_sprite_X4.png", IntRect(1500, 1143, 772, 773)))
 	{
-		cout << "Erreur durant la création de la texture" << endl;
+		cout << "Erreur durant le chargement de la texture" << endl;
 		return EXIT_FAILURE; // On ferme le programme
 	}
 
 	pac_man.setTexture(sheet);
+	fond.setTexture(sheet2);
+
 
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed)
 				window.close();
+		}
+
+		if (event.type == Event::KeyPressed)
+		{
+			switch (event.key.code)
+			{
+			case(Keyboard::Numpad4):
+				pac_man.move(Vector2f(-1, 0));
+				break;
+
+			case(Keyboard::Numpad6):
+				pac_man.move(Vector2f(1, 0));
+				break;
+
+			case(Keyboard::Numpad8):
+				pac_man.move(Vector2f(0, -1));
+				break;
+
+			case(Keyboard::Numpad2):
+				pac_man.move(Vector2f(0, 1));
+				break;
+
+			default:
+
+				break;
+			}
 		}
 
 		window.clear();
 
-
+		window.draw(fond);
 		window.draw(pac_man);
 
-		window.draw(shape);
+		//window.draw(shape);
 		window.display();
 	}
 	return 0;
